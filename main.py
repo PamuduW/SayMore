8e908c63a3f23bb47e225e4413a8fef7d5fcc6e9
PamuduW <pamuduwijesingha2k20@gmail.com>
2025-01-02 18:43:05 +0530

    test

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
     return {"message": "Backend of the SayMore app"}
-
 
 @app.get("/test/{test_type}")
 async def say_hello(test_type: int):
