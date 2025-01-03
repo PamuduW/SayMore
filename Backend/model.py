c3aa03ce1f10e459653974b26c497389269f52af
PamuduW <pamuduwijesingha2k20@gmail.com>
2025-01-03 21:44:35 +0530

    test

--- Backend/model.py
+++ Backend/model.py
@@ -1,3 +1,4 @@
+import uuid
 import torch
 import librosa
 import numpy as np
@@ -6,17 +7,13 @@ from firebase_admin import storage
 # model = torch.load("path/to/your/model.pth")
 # model.eval()
 
-def analyze_audio(file_id):
+def analyze_audio(local_file):
     try:
-        # bucket = storage.bucket()
-        # blob = bucket.blob(file_name)
-        # local_file = "/tmp/temp_audio.wav"
-        # blob.download_to_filename(local_file)
-        #
-        # y, sr = librosa.load(local_file, sr=16000)
-        # mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-        #
-        # print(mfccs)
-        return {file_id: {"stuttering": True, "confidence": 0.8}}
+        y, sr = librosa.load(local_file, sr=16000)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+
+        analysis_id = str(uuid.uuid4())
+
+        return {analysis_id: {"stuttering": True, "confidence": 0.8}},mfccs
     except Exception as e:
         return {"error": str(e)}
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials, initialize_app, storage, firestore
 from google.cloud.firestore_v1 import ArrayUnion
 import uuid
 from Backend.model import analyze_audio
+import os
 
 app = FastAPI()
 
@@ -19,20 +20,29 @@ async def root():
 @app.post("/test/{acc_id}")
 async def test(acc_id: str, file: UploadFile = File(...)):
     try:
+        ##################################################################################
         # upload the file straight from the frontend and have it send the file_name
         file_id = str(uuid.uuid4())
         file_name = f"test/{acc_id}+{file_id}.wav"
 
         bucket = storage.bucket()
         blob = bucket.blob(file_name)
-        blob.upload_from_string(await file.read(), content_type=file.content_type)
+        blob.upload_from_file(file.file, content_type="audio/wav")
+        ####################################################################################
 
-        analysis_result = analyze_audio(file_id)
+        os.makedirs('Temp', exist_ok=True)
+        local_file = "Temp/temp_audio.wav"
+
+        bucket = storage.bucket()
+        blob = bucket.blob(file_name)
+        blob.download_to_filename(local_file)
+
+        analysis_result, mfccs = analyze_audio(local_file)
 
         doc_ref = db.collection("User_Accounts").document(acc_id)
         doc_ref.update({"results": ArrayUnion([analysis_result])})
 
         # blob.delete()
-        return {"result": analysis_result}
+        return {"result": analysis_result, "mfccs": str(mfccs)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
