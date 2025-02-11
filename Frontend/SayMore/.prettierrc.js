029268dd655d0b64c78c3bfaab6637924fa6bc76
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2025-02-11 21:57:44 +0530

    chore: Update eslint and prettier config
    This commit updates eslint, prettier, and related dependencies,
    and configures prettier and eslint to work together.

--- Frontend/SayMore/.prettierrc.js
+++ Frontend/SayMore/.prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
+  bracketSpacing: true,
+  singleQuote: false,
   trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
 };
