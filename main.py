14d68bd50509eae3a9a13e9887ee63df4714be02
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2025-02-13 13:10:25 +0530

    feat: main.py: Update test endpoint to use request body
    This commit modifies the `/test` endpoint in `main.py` to accept a request body containing the file name and account ID, instead of directly taking a file. It also adds pydantic.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
 from firebase_admin import credentials, initialize_app, storage, firestore
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from google.cloud.firestore_v1 import ArrayUnion
-import uuid
 from Backend.model import analyze_audio
+from pydantic import BaseModel
 import os
+
+# import uuid
 
 app = FastAPI()
 
@@ -12,23 +14,31 @@ initialize_app(cred, {"storageBucket": "saymore-340e9.firebasestorage.app"})
 db = firestore.client()
 
 
+class RequestBody(BaseModel):
+    file_name: str
+    acc_id: str
+
+
 @app.get("/")
 async def root():
     return {"message": "Backend with the Deep Learning model of the SayMore app"}
 
 
-@app.post("/test/{acc_id}")
-async def test(acc_id: str, file: UploadFile = File(...)):
+@app.post("/test")
+async def test(request_body: RequestBody):
     try:
-        ##################################################################################
-        # upload the file straight from the frontend and have it send the file_name
-        file_id = str(uuid.uuid4())
-        file_name = f"test/{acc_id}+{file_id}.wav"
+        # ##################################################################################
+        # # upload the file straight from the frontend and have it send the file_name
+        # file_id = str(uuid.uuid4())
+        # file_name = f"test/{acc_id}+{file_id}.wav"
+        #
+        # bucket = storage.bucket()
+        # blob = bucket.blob(file_name)
+        # blob.upload_from_file(file.file, content_type="audio/wav")
+        # ####################################################################################
 
-        bucket = storage.bucket()
-        blob = bucket.blob(file_name)
-        blob.upload_from_file(file.file, content_type="audio/wav")
-        ####################################################################################
+        file_name = request_body.file_name
+        acc_id = request_body.acc_id
 
         os.makedirs('Temp', exist_ok=True)
         local_file = "Temp/temp_audio.wav"
@@ -37,12 +47,12 @@ async def test(acc_id: str, file: UploadFile = File(...)):
         blob = bucket.blob(file_name)
         blob.download_to_filename(local_file)
 
-        analysis_result= analyze_audio(local_file)
+        analysis_result = analyze_audio(local_file)
 
         doc_ref = db.collection("User_Accounts").document(acc_id)
         doc_ref.update({"results": ArrayUnion([analysis_result])})
 
-        blob.delete()
+        # blob.delete()
         return {"result": analysis_result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
