4c9d75b035a8e4d91c79f6b41cbc7d92c12376a6
Disini Ruhansa Kodagoda Hettige <disinihettige@gmail.com>
2025-02-26 10:22:24 +0530

    color lines

--- Frontend/SayMore/screens/HomeScreen.tsx
+++ Frontend/SayMore/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import { useNotifications } from "../components/Notifications";
+import { NavigationProp } from '@react-navigation/native';
 
 /**
  * HomeScreen component.
@@ -9,7 +10,11 @@ import { useNotifications } from "../components/Notifications";
  * @param {Object} navigation - The navigation object used to navigate between screens.
  * @returns {JSX.Element} The rendered HomeScreen component.
  */
-const HomeScreen = ({ navigation }) => {
+interface HomeScreenProps {
+  navigation: NavigationProp<any>;
+}
+
+const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
   useNotifications();
 
   /**
@@ -18,7 +23,7 @@ const HomeScreen = ({ navigation }) => {
    *
    * @param {string} option - The selected test option.
    */
-  const handlePress = option => {
+  const handlePress = (option: string) => {
     const isPublicSpeaking = option === "Public Speaking";
     navigation.navigate("Audio", { isPublicSpeaking });
   };
@@ -49,33 +54,33 @@ const HomeScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "#F0F8FF", padding: 20 },
-  header: { marginTop: 20, alignItems: "center" },
-  greeting: { fontSize: 24, fontWeight: "bold", color: "#003366" },
-  welcomeMessage: { fontSize: 18, color: "#003366" },
+  header: { marginTop: 40, alignItems: "center" },
+  greeting: { fontSize: 30, fontWeight: "bold", color: "#003366" },
+  welcomeMessage: { fontSize: 22, color: "#003366" },
   testContainer: {
-    marginTop: 40,
+    marginTop: 60,
     alignItems: "center",
     backgroundColor: "#FFFFFF",
     borderRadius: 20,
-    padding: 20,
+    padding: 40,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 3,
     elevation: 5,
   },
-  testHeading: { fontSize: 20, fontWeight: "bold", marginBottom: 20 },
-  testImage: { width: 150, height: 150, marginBottom: 20 },
-  testOptions: { flexDirection: "row", justifyContent: "space-between", width: "100%" },
+  testHeading: { fontSize: 30, fontWeight: "bold", marginBottom: 25 },
+  testImage: { width: 200, height: 200, marginBottom: 30 },
+  testOptions: { flexDirection: "row", justifyContent: "space-between", width: "115%" },
   optionButton: {
     flex: 1,
     backgroundColor: "#87CEEB",
     borderRadius: 10,
-    padding: 15,
+    padding: 10,
     alignItems: "center",
     marginHorizontal: 10,
   },
-  optionText: { color: "#003366", fontWeight: "bold" },
+  optionText: { color: "#003366", fontWeight: "bold", fontSize: 15 },
 });
 
 export default HomeScreen;
