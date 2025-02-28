dab3594febd0c74867c1e32240da1f11b1eb32bc
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2025-02-28 12:10:48 +0530

    feat: speech_to_text.py: Add language support
    The code changes introduce language support for speech to text conversion.
    Added the ability to select the language using lan_flag.
    Also increased the timeout limit to 300s.
    logic.py: call stt with language selection

--- src/logic.py
+++ src/logic.py
@@ -20,5 +20,5 @@ def ps_test(audio_path):
     return {
         # "Voice Quality & Stability Data": analyze_speech_1(audio_path),
         # "Speech Intensity & Energy Data": analyze_speech_2(audio_path)
-        "text": transcribe_gcs(gcs_uri,True)
+        "text": transcribe_gcs(gcs_uri,True, "si")
     }
--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -4,22 +4,29 @@ import os
 
 # gcs_uri = f"gs://saymore-340e9.firebasestorage.app/{audio_path}"
 
-def transcribe_gcs(gcs_uri: str, long_flag) -> dict:
+def transcribe_gcs(gcs_uri: str, long_flag, lan_flag) -> dict:
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "saymore-340e9-firebase-adminsdk-aaxo4-2e6ac8d48e.json"
 
     client = speech.SpeechClient()
+
+    if lan_flag == "en":
+        first_language = "es-ES"
+    elif lan_flag == "si":
+        first_language = "si-LK"
+    else:
+        first_language = "ta-LK"
 
     audio = speech.RecognitionAudio(uri=gcs_uri)
     config = speech.RecognitionConfig(
         # encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
         # sample_rate_hertz=44100,
-        language_code="en-US",
+        language_code=first_language,
     )
 
-    if long_flag == True:
+    if long_flag:
         operation = client.long_running_recognize(config=config, audio=audio)
         print("Waiting for operation to complete...")
-        response = operation.result(timeout=90)
+        response = operation.result(timeout=300)
     else:
         response = client.recognize(config=config, audio=audio)
 
