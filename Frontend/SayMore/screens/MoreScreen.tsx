6f9d0051b5957c7a1ceec5fbe32a1e05ce40542a
Kanila Gunasekara <kanilagroyal@gmail.com>
2025-03-18 12:05:37 +0530

    final

--- Frontend/SayMore/screens/MoreScreen.tsx
+++ Frontend/SayMore/screens/MoreScreen.tsx
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 10,
   },
-  lessonIcon: { width: 100, height: 100 },
+  lessonIcon: { width: 100, height: 100,borderRadius: 30},
   lessonText: {
     fontSize: 20,
     fontWeight: 'bold',
