432790f136581a2dd0b7dbc4ec470a900137d3d2
Pamudu Wijesingha <155348391+PamuduW@users.noreply.github.com>
2025-03-24 07:52:11 +0530

    feat(Analysis_S): Display analysis results and loading state
    Implements loading state and displays analysis results including a chart.

--- Frontend/SayMore/components/Analysis_S.tsx
+++ Frontend/SayMore/components/Analysis_S.tsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { Text, StyleSheet, ScrollView, View, Button } from 'react-native';
+import {
+  Text,
+  StyleSheet,
+  ScrollView,
+  View,
+  Button,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { ProgressChart } from 'react-native-chart-kit';
+import LinearGradient from 'react-native-linear-gradient';
+import { useTheme } from '../components/ThemeContext';
+
+const screenWidth = Dimensions.get('window').width;
+const parentViewWidth = screenWidth - 70;
 
 interface AnalysisSProps {
   filename: string;
@@ -8,9 +22,40 @@ interface AnalysisSProps {
   type: string;
 }
 
+interface AnalysisResult {
+  language: string;
+  stutter_count: number;
+  stuttered_words: Array<{
+    word: string;
+    type: string;
+  }>;
+  cluttering_detected: boolean;
+  fluency_score: number;
+  stuttering_score: number;
+  dynamic_feedback: string;
+  confidence_score: number;
+  transcript: string;
+}
+
 const Analysis_S: React.FC<AnalysisSProps> = ({ filename, acc_id, type }) => {
   const [responseData, setResponseData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
+  const [data, setData] = useState({
+    labels: ['Energy', 'Voice', 'Final'],
+    data: [0, 0, 0],
+  });
+  const chartConfig = {
+    backgroundGradientFrom: 'transparent',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: 'transparent',
+    backgroundGradientToOpacity: 0,
+    color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+    strokeWidth: 2,
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false,
+  };
+  const theme = useTheme();
 
   useEffect(() => {
     const sendPostRequest = async () => {
@@ -21,7 +66,6 @@ const Analysis_S: React.FC<AnalysisSProps> = ({ filename, acc_id, type }) => {
           test_type: type,
           lan_flag: 'language',
         };
-
         const response = await fetch(
           'https://saymore-monorepo-8d4fc9b224ef.herokuapp.com/test',
           {
@@ -31,9 +75,8 @@ const Analysis_S: React.FC<AnalysisSProps> = ({ filename, acc_id, type }) => {
           }
         );
 
-        if (!response.ok) {
+        if (!response.ok)
           throw new Error(`HTTP error! status: ${response.status}`);
-        }
 
         const responseInfo = await response.json();
 
@@ -50,20 +93,29 @@ const Analysis_S: React.FC<AnalysisSProps> = ({ filename, acc_id, type }) => {
         }
 
         setResponseData(responseInfo);
+        setData({
+          labels: ['Fluency', 'Confidence', 'Stuttering'],
+          data: [
+            responseInfo.result.fluency_score / 100,
+            responseInfo.result.confidence_score / 100,
+            responseInfo.result.stuttering_score / 100,
+          ],
+        });
       } catch (error) {
         //console.error('Error sending POST request:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
     sendPostRequest();
-  }, [filename, acc_id, type]);
+  }, [filename, acc_id, type, navigation]);
 
   const handleNext = () => {
     if (responseData) {
-      const { result } = responseData;
-      const stutter_feedback = result.dynamic_feedback;
-      const stutter_count = result.stutter_count;
-      const stutter_score = result.stutter_score;
+      const { result } = responseData as AnalysisResult;
+
+      const { dynamic_feedback, stutter_count, stutter_score } = result;
 
       navigation.navigate('FeedbackScreen_S', {
         stutter_feedback,
@@ -73,8 +125,22 @@ const Analysis_S: React.FC<AnalysisSProps> = ({ filename, acc_id, type }) => {
     }
   };
 
+    if (loading) {
+      return (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="blue" />
+          <Text>Analyzing Speech...</Text>
+        </View>
+      );
+    }
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <LinearGradient
+      colors={
+        theme === 'dark' ? ['#1C1C1C', '#3A3A3A'] : ['#577BC1', '#577BC1']
+      }
+      style={styles.container}>
+      <ScrollView contentContainerStyle={styles.content}>
       <Text>Analysis Screen</Text>
       {responseData && (
         <View>
@@ -87,20 +153,18 @@ const Analysis_S: React.FC<AnalysisSProps> = ({ filename, acc_id, type }) => {
         </View>
       )}
     </ScrollView>
+        </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
     padding: 16,
   },
-  jsonText: {
-    fontSize: 14,
-    fontFamily: 'monospace',
-    marginVertical: 10,
+  content: {
+    paddingBottom: 40,
+    paddingTop: 50,
   },
 });
 
